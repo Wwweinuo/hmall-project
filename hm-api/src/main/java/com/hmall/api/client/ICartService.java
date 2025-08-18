aab4cd5440d16a9a6cdd767dfa6c799f77675142
“Wwweinuo” <2992180144@qq.com>
2025-08-18 17:31:07 +0800

    拆分交易微服务

---
+++ hm-api/src/main/java/com/hmall/api/client/ICartService.java
@@ -0,0 +1,18 @@
+package com.hmall.api.client;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
+import java.util.List;
+
+@FeignClient("cart-service")
+public interface ICartService {
+
+    @DeleteMapping
+    public void deleteCartItemByIds(@RequestParam("ids") Collection<Long> ids);
+
+}
